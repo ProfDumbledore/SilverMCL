6ff6e88410f131885b6670cf36f77e74d2d16a75
ProfDumbledore <mike@logicwave.com>
2020-10-15 12:25:25 -0400

    First time working

---
+++ MCLsharp/Class1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Text;
+using Crestron.SimplSharp;      // For Basic SIMPL# Classes
+using Crestron.SimplSharp.CrestronXml;
+using Crestron.SimplSharp.CrestronXmlLinq;
+using Crestron.SimplSharp.CrestronIO;
+
+
+
+
+namespace MCLsharp
+{
+    public class Reader
+    {
+        string strPath = "\\NVRAM\\today.xml";
+
+
+
+        public Reader()
+        {
+        }
+
+        public void Go()
+        {
+            int x = 0;
+   
+            var myStream = new FileStream(strPath, FileMode.Open);
+
+            var myXMLReader = new XmlReader(myStream);
+
+            while (myXMLReader.Read())
+
+            {
+
+            x = x++;
+
+            if (x > 400)
+
+            throw new Exception("Looped 400 Times failed to EOF");
+
+            switch (myXMLReader.NodeType)
+
+            {
+
+            case XmlNodeType.Element:
+
+            switch (myXMLReader.Name.ToLower())
+
+            {
+
+            case "book":
+
+            if (myXMLReader.HasAttributes)
+
+            {
+
+            while (myXMLReader.MoveToNextAttribute())
+
+            {
+
+            if (myXMLReader.Name.ToLower() == "id")
+
+            CrestronConsole.PrintLine("ID: {0}", myXMLReader.Value);
+
+            }
+
+            }
+
+            break;
+
+            case "author":
+
+            case "title":
+
+            case "genre":
+
+            case "description":
+
+            CrestronConsole.PrintLine("{0}: {1}", myXMLReader.Name, myXMLReader.ReadString());
+
+            break;
+
+            }
+
+            break;
+
+            default:
+
+            break;
+
+            }
+        
+        }
+    }
+}
+}
---
+++ MCLsharp/Properties/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyTitle("MCLsharp")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MCLsharp")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyVersion("1.0.0.*")]
+
